5c29f3e11671d6980fe6d48b542a87a17887f21f
jaosch <jasper.schommartz@gmail.com>
2024-06-26 22:50:17 +0200

    Initial comming: 1D viscoelasticity

---
+++ 1D_material_modeling/viscoelasticity/src/models.py
@@ -0,0 +1,140 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+def make_layer(m_type, params, **kwargs):
+    """ Factory method that calls and returns layer object """
+    cf = {
+        'Maxwell': MaxwellAnalyticalCell,
+        'Naive': NaiveRNNCell,
+        'GSM': GSMCell
+        }
+    class_obj = cf.get(m_type, None)
+    if class_obj:
+        return class_obj(params, **kwargs)
+    raise ValueError('Unknown class type')
+
+class MLP(layers.Layer):
+    ''' A feed-forward neural network '''
+    def __init__(self, units, activation, non_neg):
+        super().__init__()
+        self.ls = []
+        for (u, a, n) in zip(units, activation, non_neg):
+            if n:
+                kernel_constraint = tf.keras.constraints.non_neg()
+            else:
+                kernel_constraint = None
+            self.ls += [layers.Dense(u, a, kernel_constraint=kernel_constraint)]  
+
+    def call(self, x):    
+        for l in self.ls:
+            x = l(x)
+        return x
+    
+
+class MaxwellAnalyticalCell(layers.Layer):
+    ''' Fully analytical Maxwell model with explicit Euler time integration '''
+    def __init__(self, params, **kwargs):
+        super().__init__()
+        self.state_size = [[1]]
+        self.output_size = [[1]]
+     
+        self.E_infty = params['E_infty']
+        self.E = params['E']
+        self.eta = params['eta']
+        
+    def call(self, inputs, states):
+        '''   states are the internal variables
+           n: current time step, N: old time step '''
+        eps_N = inputs[0]
+        hs_N = inputs[1]
+        gamma_N = states[0]
+        
+        # compute stress
+        sig_N = self.E_infty * eps_N + self.E * (eps_N - gamma_N)
+
+        # integrate internal variable for next time step
+        gamma_n = gamma_N + hs_N * self.E/self.eta * (eps_N - gamma_N)
+                
+        return sig_N, [gamma_n]
+    
+    def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
+        ''' Define the initial values of the internal variables. '''     
+        return [tf.zeros([batch_size, 1])]
+    
+class NaiveRNNCell(layers.Layer):
+    ''' Naive RNN model with fully NN-based material model and time integration '''
+    def __init__(self, params, **kwargs):
+        super().__init__()
+        self.state_size = [[1]]
+        self.output_size = [[1]]
+     
+        self.mlp = MLP(**kwargs)
+        
+    def call(self, inputs, states):
+        ''' states are the internal variables
+            n: current time step, N: old time step '''
+
+        # concatenate NN inputs   
+        eps_N = inputs[0]
+        hs_N = inputs[1]
+        gamma_N = states[0]
+        x = tf.concat([eps_N, hs_N, gamma_N], axis = 1)
+
+        # Evaluate NN 
+        x = self.mlp(x)
+         
+        sig_N = x[:, 0:1]
+        gamma_n = x[:, 1:2]
+        
+        return sig_N, [gamma_n]
+    
+    def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
+        ''' Define the initial values of the internal variables.'''  
+        return [tf.zeros([batch_size, 1])]
+    
+
+class GSMCell(layers.Layer):
+    ''' GSM cell '''
+    def __init__(self, params, **kwargs):
+        super().__init__()
+        self.state_size = [[1]]
+        self.output_size = [[1]]
+        self.eta = params['eta']
+
+        self.mlp = MLP(**kwargs)
+        
+    def call(self, inputs, states):
+        '''  states are the internal variables
+        n: current time step, N: old time step '''
+                
+        eps_N = inputs[0]
+        hs_N = inputs[1]
+        gamma_N = states[0]
+        x = tf.concat([eps_N, gamma_N], axis = 1)
+        
+        with tf.GradientTape(persistent=True) as g:
+            g.watch(x)
+            y = self.mlp(x)
+        dedgamma = g.gradient(y, x)
+
+        sig_N = dedgamma[:, 0, tf.newaxis]
+        gamma_n = gamma_N - hs_N * self.eta**(-1) * dedgamma[:, 1, tf.newaxis]
+                
+        return sig_N , [gamma_n]
+    
+    def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
+        ''' Define initial values of the internal variables. '''
+        return [tf.zeros([batch_size, 1])]
+
+def build(m_type, params, **kwargs):
+    ''' Build the model. '''
+    eps = tf.keras.Input(shape=[None, 1])
+    hs = tf.keras.Input(shape=[None, 1])
+        
+    cell = make_layer(m_type, params, **kwargs)
+    rnn = layers.RNN(cell, return_sequences=True, return_state=False)
+    sigs = rnn((eps, hs))
+
+    model = tf.keras.Model([eps, hs], [sigs])
+    model.compile('adam', 'mse')
+    return model
---
+++ 1D_material_modeling/viscoelasticity/src/plots.py
@@ -0,0 +1,96 @@
+"""
+Tutorial Machine Learning in Solid Mechanics (WiSe 22/23)
+Task 4: Viscoelasticity
+==================
+Authors: Dominik K. Klein
+         
+01/2023
+"""
+
+
+# %%   
+"""
+Import modules
+
+"""
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+colors = np.array([
+        'tab:blue', 'tab:orange', 'tab:green',
+        'tab:red', 'tab:purple', 'tab:brown',
+        'tab:pink', 'tab:gray', 'tab:olive'
+        ])
+
+def plot_loss(history):
+    fig = plt.figure(1, dpi=600)
+    plt.semilogy(history['loss'], color='tab:blue', label='training loss')
+    #plt.semilogy(history['val_loss'], color='tab:blue', linestyle='--', label='validation loss')
+    plt.grid(which='both')
+    plt.xlabel('calibration epoch')
+    plt.ylabel('log$_{10}$ MSE')
+    plt.legend()
+
+def plot_data(eps, eps_dot, sig, omegas, As):
+    n = len(eps[0])
+    ns = np.linspace(0, 2*np.pi, n)
+    
+    fig = plt.figure(dpi = 600, figsize = (10, 8))
+    plt.subplot(2,2,1)
+    plt.title('Data')
+    for i in range(len(eps)):     
+        plt.plot(ns, sig[i], label = '$\\omega$: %.2f, $A$: %.2f' \
+                 %(omegas[i], As[i]), color=colors[i], linestyle='--')
+        plt.xlim([0, 2*np.pi])
+        plt.ylabel('stress $\\sigma$')
+        plt.xlabel('time $t$')
+        plt.legend() 
+        
+    plt.subplot(2,2,2) 
+    for i in range(len(eps)):
+        plt.plot(eps[i], sig[i], color=colors[i], linestyle='--')
+        plt.xlabel('strain $\\varepsilon$')
+        plt.ylabel('stress $\\sigma$')
+        
+    plt.subplot(2,2,3)
+    for i in range(len(eps)):
+        plt.plot(ns, eps[i], color=colors[i], linestyle='--')
+        plt.xlim([0, 2*np.pi])
+        plt.xlabel('time $t$')
+        plt.ylabel('strain $\\varepsilon$')
+        
+        
+    plt.subplot(2,2,4)
+    for i in range(len(eps)):
+        plt.plot(ns, eps_dot[i], color=colors[i], linestyle='--')
+        plt.xlim([0, 2*np.pi])
+        plt.xlabel('time $t$')
+        plt.ylabel(r'strain rate $\.{\varepsilon}$')
+    
+    
+def plot_model_pred(eps, sig, sig_pred, omegas, As):
+    n = len(eps[0])
+    ns = np.linspace(0, 2*np.pi, n)
+    
+    fig = plt.figure(dpi = 600, figsize = (10,4))
+    
+    plt.subplot(1,2,1)
+    plt.title('Data: dashed line, model prediction: continuous line')
+    for i in range(len(eps)): 
+        plt.plot(ns, sig[i], label = '$\\omega$: %.2f, $A$: %.2f'%(omegas[i], As[i]),linestyle='--', color=colors[i])
+        plt.plot(ns, sig_pred[i], color=colors[i])
+        plt.xlim([0, 2*np.pi])
+        plt.ylabel('stress $\\sigma$')
+        plt.xlabel('time $t$')
+        plt.legend()
+        
+    plt.subplot(1,2,2)
+    for i in range(len(eps)):
+        plt.plot(eps[i], sig[i], linestyle='--', color=colors[i])
+        plt.plot(eps[i], sig_pred[i], color=colors[i])
+        plt.xlabel('strain $\\varepsilon$')
+        plt.ylabel('stress $\\sigma$')
+
+    plt.show()
